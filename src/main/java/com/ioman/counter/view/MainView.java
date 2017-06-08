bef305f6ccbf1c41123936cb3385a70c23ead74a
leeis <lewis.lynn1006@gmail.com>
2017-06-08 17:22:25 +0800

    1.增加多计时器;2.修改边框样式

--- src/main/java/com/ioman/counter/view/MainView.java
+++ src/main/java/com/ioman/counter/view/MainView.java
@@ -1,7 +1,7 @@
 package com.ioman.counter.view;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -34,15 +34,15 @@ public class MainView implements ActionListener, KeyListener {
 		
 		frame = new JFrame();
 		
+		double width = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		
 		frame.setMinimumSize(new Dimension(680, 200));
-		frame.setSize(480, 200);
+		frame.setSize((int)width, (int)height);
 		frame.setTitle("慢速烘箱定时提醒器");
 		Image icon = Toolkit.getDefaultToolkit().getImage("");
 		frame.setIconImage(icon);
 		frame.setDefaultCloseOperation(3);
-		
-		double width = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-		double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 		
 		System.out.println("width = " + width + ", height = " + height);
 		
@@ -57,28 +57,61 @@ public class MainView implements ActionListener, KeyListener {
 		
 		panel = new JPanel();
 		
-		panel.setBorder(new EmptyBorder(5,5,5,5));
-		panel.setLayout(new GridLayout(1, 5));
+		int counterCount = 8;
 		
-//		JPanel timeSettingPanel = new JPanel();
-//		timeSettingPanel.setLayout(new GridLayout(1, 1));
+		panel.setBorder(new EmptyBorder(5,5,5,5));
+		panel.setLayout(new GridLayout(counterCount, 1));
+		
+		for (int i=1; i<= counterCount; i++){
+			panel.add(loadSingleCounter(i));
+		}
+		
+		frame.add(panel);
+	}
+	
+	public JPanel loadSingleCounter(int i){
+		
+		JPanel panel = new JPanel();
+		
+		panel.setBorder(new EmptyBorder(5,5,5,5));
+		panel.setLayout(new GridLayout(1, 6));
+		
+		panel.add(titlePanel(i));
 		
 		panel.add(hourPanel());
 		panel.add(minutesPanel());
-		
-//		panel.add(timeSettingPanel);
-		
-//		JPanel timeTextPanel = new JPanel();
-//		timeTextPanel.setLayout(new GridLayout(2, 1));
-//		timeTextPanel.add(usedTimeInfo());
-//		timeTextPanel.add(leftTimeInfo());
 		
 		panel.add(usedTimeInfo());
 		panel.add(leftTimeInfo());
 		
 		panel.add(controlButton());
 		
-		frame.add(panel);
+//		LineBorder lineBorder = (LineBorder)BorderFactory.createLineBorder(Color.black);// 创建线形边框
+		
+//		EtchedBorder etchedBorder = (EtchedBorder)BorderFactory.createEtchedBorder();// 创建蚀刻式边框
+//
+		BevelBorder raisedBevelBorder = (BevelBorder)BorderFactory.createRaisedBevelBorder();// 创建浮雕式边框
+//
+//		BevelBorder loweredBevelBorder = (BevelBorder)BorderFactory.createLoweredBevelBorder();// 创建下沉边框
+		
+		panel.setBorder(raisedBevelBorder);
+		
+		return panel;
+	}
+	
+	private JPanel titlePanel(int i){
+		
+		JPanel panel = new JPanel();
+		
+		panel.setBorder(new EmptyBorder(15,5,5,5));
+		panel.setLayout(new GridLayout(1, 1));
+		
+		JLabel label = new JLabel("#" + i);
+		label.setFont(new Font("Dialog", 1, 20));
+		label.setForeground(Color.RED);
+		panel.add(label);
+		
+		return panel;
 	}
 	
 	/**
@@ -110,7 +143,11 @@ public class MainView implements ActionListener, KeyListener {
 		
 		panel.setLayout(new GridLayout(1, 1));
 		
-		minuteComboBox = new JComboBox(new String[]{"1", "2", "3", "4", "5"});
+		minuteComboBox = new JComboBox(new String[]{"1", "2", "3", "4", "5", "6", "7",
+				"8", "9", "10", "11", "12", "13", "14","15", "16", "17", "18", "19", "20", "21",
+				"22", "23", "24", "25", "26", "27", "28","29", "30","31", "32", "33", "34", "35", "36", "37",
+				"38", "39", "40", "41", "42", "43", "44","45", "46", "47", "48", "49", "50", "51",
+				"52", "53", "54", "55", "56", "57", "58","59"});
 		
 		minuteComboBox.setPreferredSize(new Dimension(150, 26));
 		
